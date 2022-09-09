bdb569e0ca37a8680266839ea9b5d60df48d4725
Bradley Cardona <bradleymathematics@gmail.com>
2022-09-09 18:46:15 -0400

    add notes

---
+++ javascript/es6_modules.js
@@ -0,0 +1,29 @@
+// Helpful links:
+// The History of Javascript: https://peterxjang.com/blog/modern-javascript-explained-for-dinosaurs.html
+// WebPack: https://webpack.js.org/
+// Creating a package.json file: https://docs.npmjs.com/creating-a-package-json-file
+// Dependencies vs devDependencies: https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies
+
+
+// Using WebPack, any new JavaScript libraries will be added using require statements in the JavaScript, as opposed to adding new <script> tags in the HTML. Having a single JavaScript bundle file is often better for performance.
+
+
+// Use npm to . . .
+// Adapt packages of code for your apps, or incorporate packages as they are.
+// Download standalone tools you can use right away.
+// Run packages without downloading using npx.
+// Share code with any npm user, anywhere.
+// Restrict code to specific developers.
+// Create organizations to coordinate package maintenance, coding, and developers.
+// Form virtual teams by using organizations.
+// Manage multiple versions of code and code dependencies.
+// Update applications easily when underlying code is updated.
+// Discover multiple ways to solve the same puzzle.
+// Find other developers who are working on similar problems and projects.
+
+
+// You can add a package.json file to your package to make it easy for others to manage and install. Packages published to the registry must contain a package.json file.
+// A package.json file:
+// lists the packages your project depends on
+// specifies versions of a package that your project can use using semantic versioning rules
+// makes your build reproducible, and therefore easier to share with other developers
